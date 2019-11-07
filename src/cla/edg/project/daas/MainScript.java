6d21ddb79af86639d925f9ee65c79adf4300f3c3
Jarry Zhou <jarryzhou@doublechaintech.com>
2019-11-07 18:27:25 +0800

    daas

--- src/cla/edg/project/daas/MainScript.java
+++ src/cla/edg/project/daas/MainScript.java
@@ -12,28 +12,35 @@ public class MainScript extends BasePageFlowDescriptionScript {
 				.parent_class_name("BasicDaasService")
 				.parent_class_package("com.doublechain.daas.daasservice")
 			
-			
+			/**
+			 * 由于目前是用jsp 做的，
+			 * 为了不产生 .jsp 的url， 简单跳转页面也用脚本做
+			 */
 			.request("login").no_login()
 				.got_page("login")
-				.comments("登陆页面")
+				.comments("访问登录页面")
 				
 			.request("doLogin").no_login().with_string("email").with_string("password")
+				.comments("登录")
 				.when("error").got_page("login")
 				.when_others().got_page("download token")
 				
 			.request("registration").no_login()
-				.comments("注册页面")
+				.comments("访问注册页面")
 				.got_page("registration")
 				
 			.request("send code").no_login().with_string("email").got_page("registration")
+				.comments("发送验证码到邮箱")
 			
 			.request("doRegistration").no_login().with_string("email").with_string("password").with_string("confirmPassword").with_string("code")
+				.comments("注册")
 				.when("error").got_page("registration")
 				.when_others().got_page("welcome")
 				
 			.request("continue").got_page("login")
 			
-			.request("reset password").no_login().got_page("reset password").comments("重置密码")
+			.request("reset password").no_login().got_page("reset password")
+				.comments("重置密码")
 			
 			;
 
