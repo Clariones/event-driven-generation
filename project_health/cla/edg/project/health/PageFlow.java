49de3a6c068a5f097491df8da88f3750288ed622
Jarry Zhou <jarryzhou@doublechaintech.com>
2020-02-17 13:40:32 +0800

    health-biz-suite scripts

--- project_health/cla/edg/project/health/PageFlow.java
+++ project_health/cla/edg/project/health/PageFlow.java
@@ -11,33 +11,33 @@ public class PageFlow extends BasePageFlowDescriptionScript {
 				.parent_class_name("BasicWxappService")
 				.parent_class_package("com.doublechaintech.health.wechatapp")
 				
-				
-				
-				
-				
-				
 			.standard_homepage("健康调查")
-			.login_as("user")
+			.login_as("user").got_me_page().comments("登录后选择身份")
+				.may_request("switch to teacher")
+				.may_request("switch to student")
 			.login_from_wxapp_by_openid()
-			.got_me_page().comments("首页，只有老师有首页，包含班级列表和近期问卷列表")
-				.may_request("add class").comments("创建班级")
 			
-					
+			.request("update profile").need_login().with_string("name").with_string("avatar").with_string("userType")
+				.when("switchToTeacher")
+					.got_me_page()
+				.when_others()
+					.got_page("student survey list")
 			/**
-			 * 老师
+			 * ========================================老师========================================
 			 */
-			.request("teacher login").no_login().with_object("com.terapico.caf.baseelement.LoginParam as loginParam")
-				.got_me_page()
-				.may_request("add class")
-				.may_request("view survey detail")
-				.may_request("view class detail")
+			.request("switch to teacher")
+				.when("empty class")
+					.got_page("add class")
+				.when_others()
+					.got_me_page().comments("首页，只有老师有首页，包含班级列表和近期问卷列表")
+					.may_request("add class").comments("创建班级")
 			
 			.request("add class").got_page("add class")
 				.may_request("submit class")
 				
 			.request("submit class").comments("创建班级").need_login()
 				.with_form("teacher")
-				.got_page("survey list")
+				.got_me_page()
 			
 			
 			.request("view class detail").need_login().with_string("teacher id")
@@ -56,32 +56,31 @@ public class PageFlow extends BasePageFlowDescriptionScript {
 			
 			
 				
-			/**
-			 * 学生
-			 */
+				/**
+				 * ========================================学生========================================
+				 */
 				
-			.request("student login").no_login().with_string("survey id").comments("学生登录填写问卷")
-				.when("submitted")
-					.got_page("student survey detail")
-				.when_others()
-					.got_page("student survey form")
-					.may_request("submit student survey")
-					
+			.request("switch to student").got_page("student survey list")
+			
 			.request("student view survey").need_login().with_string("survey id").comments("学生登录后查看问卷")
 				.when("submitted")
-					.got_page("student survey detail")
+					.got_page("student survey list").list_of("student_health_survey")
+					.may_request("view student survey detail")
 				.when_others()
 					.got_page("student survey form")
 				.may_request("submit student survey")
+				
+			.request("view student survey detail").with_string("student survey id").got_page("student survey detail")
 					
-			.request("submit student survey").need_login().with_string("survey id").with_string("answer").with_string("student name").got_page("student survey detail")
+			.request("submit student survey").need_login().with_string("survey id").with_string("answer").with_string("student name").got_page("student survey list")
 			
 			;
 		
 
 	@Override
 	public PageFlowScript getScript() {
-		return SCRIPT.import_from(new Query())
+		return SCRIPT
+				.import_from(new Query())
 				;
 	}
 
--- project_health/cla/edg/project/health/Query.java
+++ project_health/cla/edg/project/health/Query.java
@@ -8,7 +8,7 @@ public class Query extends PieceOfScript {
 	@Override
 	public PageFlowScript makeSequel(PageFlowScript script) {
 		return script
-
+				
 				.find(MODEL.studentHealthSurvey().getModelTypeName())
 					.which("student by survey id")
 					.with_string("survey id")
@@ -16,17 +16,54 @@ public class Query extends PieceOfScript {
 					
 					.do_it_as()
 					.where(MODEL.studentHealthSurvey().classDailyHealthSurvey().eq("${survey id}"),
-							MODEL.studentHealthSurvey().student().user().eq("${user id}"))
+							MODEL.studentHealthSurvey().student().user().eq("${user id}"),
+							MODEL.studentHealthSurvey().surveyStatus().eq(MODEL.surveyStatus().UN_SUBMITTED))
 					.wants(MODEL.studentHealthSurvey().studentDailyAnswerList(),MODEL.studentHealthSurvey().studentDailyAnswerList().question())
+					
+				.find(MODEL.studentHealthSurvey().getModelTypeName())
+					.which("is submitted by student and survey id")
+					.with_string("survey id")
+					.with_string("user id")
+					
+					.do_it_as()
+					.where(MODEL.studentHealthSurvey().classDailyHealthSurvey().eq("${survey id}"),
+							MODEL.studentHealthSurvey().student().user().eq("${user id}"),
+							MODEL.studentHealthSurvey().surveyStatus().eq(MODEL.surveyStatus().SUBMITTE))
+					.wants(MODEL.studentHealthSurvey().studentDailyAnswerList(),MODEL.studentHealthSurvey().studentDailyAnswerList().question())
+					
+				.query(MODEL.studentHealthSurvey().getModelTypeName())
+					.which("student by survey id")
+					.with_string("survey id")
+					.do_it_as()
+					.where(MODEL.studentHealthSurvey().classDailyHealthSurvey().eq("${survey id}"),
+							MODEL.studentHealthSurvey().surveyStatus().eq(MODEL.surveyStatus().SUBMITTE))
 
 				.find(MODEL.student().getModelTypeName()).which("name is").with_string("student name").with_string("user id")
 					.do_it_as()
 					.where(MODEL.student().studentName().eq("${student name}"),MODEL.student().user().eq("${user id}"))
 					
-				.query(MODEL.studentHealthSurvey()).which("is submitted by teacher id").with_string("teacher id")
+				.query(MODEL.studentHealthSurvey().getModelTypeName()).which("is submitted by teacher id").with_string("teacher id")
 					.do_it_as()
 					.where(MODEL.studentHealthSurvey().teacher().eq("${teacher id}"),MODEL.studentHealthSurvey().surveyStatus().eq(MODEL.surveyStatus().SUBMITTE))
-		;
+					
+				.query(MODEL.studentHealthSurvey().getModelTypeName()).list_of("user").with_string("user id")
+					.do_it_as()
+					.where(MODEL.studentHealthSurvey().student().user().eq("${user id}"))
+					.order_by(MODEL.studentHealthSurvey().answerTime()).desc()
+					.wants(MODEL.studentHealthSurvey().studentDailyAnswerList(),MODEL.studentHealthSurvey().studentDailyAnswerList().question(),MODEL.studentHealthSurvey().studentDailyAnswerList().question().questionType(),MODEL.studentHealthSurvey().student())
+					
+				.find(MODEL.classDailyHealthSurvey().getModelTypeName()).list_of("id").with_string("survey id")
+					.do_it_as()
+					.where(MODEL.classDailyHealthSurvey().id().eq("${survey id}"))
+					.wants(MODEL.classDailyHealthSurvey().studentHealthSurveyList().studentDailyAnswerList().question().questionType(),MODEL.classDailyHealthSurvey().dailySurveyQuestionList(),MODEL.classDailyHealthSurvey().studentHealthSurveyList().student(),MODEL.classDailyHealthSurvey().teacher())
+		
+				.find(MODEL.classDailyHealthSurvey().getModelTypeName()).list_of("last time").with_string("teacher id").with_date("current survey time")
+					.do_it_as()
+					.where(MODEL.classDailyHealthSurvey().teacher().eq("${teacher id}"),MODEL.classDailyHealthSurvey().surveyTime().less("${current survey time}"))
+					.order_by(MODEL.classDailyHealthSurvey().surveyTime()).desc()
+					.wants(MODEL.classDailyHealthSurvey().studentHealthSurveyList().student())
+					
+				;
 
 	}
 }
